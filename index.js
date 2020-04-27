14fcc666932bf95697eb401f2e7f28758089398d
Karolina Chodorek <giemolla@MacBook-Pro-Kamil.local>
2020-04-27 12:40:12 +0200

    css refactored using sass, main page finished, up-button behavior added, managed html generation using js

---
+++ index.js
@@ -0,0 +1,132 @@
+const navItems = [
+	"kwiaty",
+	"warzywa",
+	"zioła",
+	"sklep online",
+	"sklepy stacjonarne",
+	"porady",
+	"aranzacje",
+	"kontakt"
+];
+
+const categories = [
+	{ name: "kwiaty", imageUrl: "./images/petunia.jpeg" },
+	{ name: "warzywa", imageUrl: "./images/pomidor.jpg" },
+	{ name: "zioła", imageUrl: "./images/bazylia.jpg" }
+];
+
+const articles = [
+	{
+		id: 1,
+		title: "Kwiaty na zacieniony balkon",
+		content:
+			"Proin lacinia condimentum ipsum, iaculis viverra metus rhoncus eu. Etiam vitae sodales urna. Nulla consequat nulla sed nisl congue, vel suscipit mauris pharetra. Curabitur elit dolor, vehicula quis ligula in, porta feugiat lectus. In eget mi maximus, fermentum lorem eget, scelerisque ex. Sed viverra quam in libero congue fermentum...",
+		imageUrl: "./images/cienie.jpg"
+	},
+	{
+		id: 2,
+		title: "Kwiaty na zacieniony balkon",
+		content:
+			"Proin lacinia condimentum ipsum, iaculis viverra metus rhoncus eu. Etiam vitae sodales urna. Nulla consequat nulla sed nisl congue, vel suscipit mauris pharetra. Curabitur elit dolor, vehicula quis ligula in, porta feugiat lectus. In eget mi maximus, fermentum lorem eget, scelerisque ex. Sed viverra quam in libero congue fermentum...",
+		imageUrl: "./images/img_0008.jpg"
+	},
+	{
+		id: 3,
+		title: "Kwiaty na zacieniony balkon",
+		content:
+			"Proin lacinia condimentum ipsum, iaculis viverra metus rhoncus eu. Etiam vitae sodales urna. Nulla consequat nulla sed nisl congue, vel suscipit mauris pharetra. Curabitur elit dolor, vehicula quis ligula in, porta feugiat lectus. In eget mi maximus, fermentum lorem eget, scelerisque ex. Sed viverra quam in libero congue fermentum...",
+		imageUrl: "./images/ziola-na-balkonie.jpg"
+	}
+];
+
+const navList = document.querySelector(".nav-list");
+const categoriesNode = document.querySelector(".categories");
+const articleSection = document.querySelector(".article-section");
+const gallery = document.querySelector(".grid-gallery");
+
+const categoryTemplate = function(item, i) {
+	const { name } = item;
+	return `
+        <div class="category">
+            <div class="category-pic pic-${i + 1}"></div>
+            <button class="primary-button">${name.toUpperCase()}</button>
+        </div>
+    `;
+};
+
+const articleTemplate = function(item, i) {
+	const { title, content } = item;
+	return `
+        <div class="article">
+            <h3 class="article-header">${title}</h3>
+            <p class="article-text">${content}</p>
+            <a href="#">Czytaj dalej ></a>
+        </div>
+        <div class="article-image-wrapper">
+            <div class="article-image img-${i + 1}"></div>
+        </div>
+    `;
+};
+
+function generateRecurrentElements(array, elementType, cssClass, parent) {
+	array.forEach((item, i) => {
+		const recurrentElement = document.createElement(elementType);
+		recurrentElement.classList.add(cssClass);
+		if (cssClass === "nav-item") {
+			recurrentElement.innerHTML = item;
+		}
+		if (cssClass === "category") {
+			recurrentElement.innerHTML = categoryTemplate(item, i);
+		}
+		if (cssClass === "article-wrapper") {
+			recurrentElement.innerHTML = articleTemplate(item, i);
+		}
+		if (cssClass === "photo") {
+			recurrentElement.classList.add(`photo-${i + 1}`);
+		}
+		parent.appendChild(recurrentElement);
+	});
+}
+
+const generateNav = generateRecurrentElements(
+	navItems,
+	"li",
+	"nav-item",
+	navList
+);
+
+const generateCategories = generateRecurrentElements(
+	categories,
+	"div",
+	"category",
+	categoriesNode
+);
+
+const generateArticles = generateRecurrentElements(
+	articles,
+	"article",
+	"article-wrapper",
+	articleSection
+);
+
+const generateGallery = generateRecurrentElements(
+	[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+	"div",
+	"photo",
+	gallery
+);
+
+const arrowUp = document.querySelector(".up-button");
+
+arrowUp.addEventListener("click", function(e) {
+	e.preventDefault();
+	window.scroll({ top: 0, behavior: "smooth" });
+});
+
+window.addEventListener("scroll", function(e) {
+	if (window.scrollY > 0) {
+		arrowUp.classList.remove("hidden");
+	} else {
+		arrowUp.classList.add("hidden");
+	}
+});
